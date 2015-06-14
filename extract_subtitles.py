fe51e9878ed3c5f0ab6e1da69fa33a04068c4b6f
dtk <dtk@gmx.de>
2015-06-15 00:36:00 +0200

    PoC
    Identify subtitle tracks.

---
+++ extract_subtitles.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+
+import re
+
+import sh
+
+
+MKV_FILE = '<matroska file to extract subtitles from>'
+
+
+if __name__ == '__main__':
+
+    mkvinfo = sh.mkvinfo(MKV_FILE)
+
+    segment_tracks = re.search(
+        '^\|\+ Segment tracks\n'
+        '^(.*?)'
+        '^\|\+ ',
+        mkvinfo.stdout,
+        re.MULTILINE | re.DOTALL
+    ).group(1)
+
+    tracks = segment_tracks.split('| + A track\n')
+    tracks = filter(None, tracks)
+
+    for track in tracks:
+        if re.search('Track type: subtitles', track):
+            track_num = re.search(
+                'Track number: \d+'
+                ' \(track ID for mkvmerge & mkvextract: (\d+)\)\n',
+                track
+            ).group(1)
+
+            language = re.search(
+                'Language: (.+)',
+                track
+            ).group(1)
+
+            name_field = re.search(
+                'Name: (.+)',
+                track
+            )
+            name = name_field.group(1) if name_field else ''
+
+            print(track_num, language, name)
